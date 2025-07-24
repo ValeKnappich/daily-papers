1bf32ffe4b6dca0ec664abe13af8ec9b75bef98c
Valentin Knappich (CR/AIR3) <Valentin.Knappich@de.bosch.com>
2025-07-24 10:47:35 +0200

    fix base path

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 
 // Update these if your repo is not served from the root (e.g., /<repo-name>)
-const repoName = "ui"; // Change to your repo name if needed
+const repoName = "daily-papers";
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.GITHUB_PAGES ? `/${repoName}` : undefined,
-  assetPrefix: process.env.GITHUB_PAGES ? `/${repoName}/` : undefined,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
