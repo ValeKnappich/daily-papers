01445be72c257024e9f780dddf4f7f7862fe2646
Valentin Knappich (CR/AIR3) <Valentin.Knappich@de.bosch.com>
2025-08-13 19:12:41 +0200

    add git commit in action & fix history

--- populate_history.py
+++ populate_history.py
@@ -16,5 +16,5 @@ interval_days = config["arxiv"]["interval_days"]
 
 for days_ago in range(interval_days, DAYS + 1, interval_days):
     date = now - timedelta(days=days_ago)
-    print(f"Fetching papers for {date.strftime('%Y-%m-%d')}")
+    print(f"\nFetching papers for {date.strftime('%Y-%m-%d')}")
     get_daily_papers_main(now=date)
